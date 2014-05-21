a0ec7a6e403ec48773d2e61399e35857fe0f36bb
Jeremy Perry <jperry@phluant.com>
2014-05-21 16:07:49 -0700

    Ensured Dojo async load will work correctly when loading scripts which explicitly require a callback (i.e. Google Maps).

--- dojo_asynch_load.js
+++ dojo_asynch_load.js
@@ -1,14 +1,22 @@
 var insert_before = document.getElementsByTagName('script')[0];
 var insert_parent = insert_before.parentNode;
 var onload_count = 0;
+var scripts = asynchLoad.scripts;
 for(var i=0; i<scripts.length; i++){
 	var new_script = document.createElement('script');
 	new_script.src = scripts[i];
 	insert_parent.insertBefore(new_script, insert_before);
-	new_script.onload = function(){
-		onload_count++;
-		if(onload_count = scripts.length){
-			preInit();
+	if(scripts[i].indexOf('callback=') == -1){
+		new_script.onload = function(){
+			scriptLoaded();
 		}
-	};
+	}	
+}
+scriptLoaded = function(){
+	onload_count++;
+	console.log(onload_count);
+	if(onload_count == scripts.length && typeof(asynchLoad.callback) != 'undefined'){
+		console.log('about to callback');
+		asynchLoad.callback();
+	}
 }
