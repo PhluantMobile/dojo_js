1d69f5f7896b786cdd88c00c3950f4b60b05d138
Jeremy Perry <jperry@phluant.com>
2014-05-21 15:18:48 -0700

    ...

--- dojo_asynch_load.js
+++ dojo_asynch_load.js
@@ -1,16 +1,13 @@
-var insert_array = ['http://mdn4.phluantmobile.net/jslib/dojo_js/dojo.min.js']
-var insert_before = document.getElementsByClassName('holder')[0];
-console.log(insert_before.getAttribute('data-script'));
-insert_array.push(insert_before.getAttribute('data-script'));
+var insert_before = document.getElementsByTagName('script')[0];
 var insert_parent = insert_before.parentNode;
 var onload_count = 0;
-for(var i=0; i<insert_array.length; i++){
+for(var i=0; i<scripts.length; i++){
 	var new_script = document.createElement('script');
-	new_script.src = insert_array[i];
+	new_script.src = scripts[i];
 	insert_parent.insertBefore(new_script, insert_before);
 	new_script.onload = function(){
 		onload_count++;
-		if(onload_count = insert_array.length){
+		if(onload_count = scripts.length){
 			preInit();
 		}
 	};
