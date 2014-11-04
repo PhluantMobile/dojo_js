7017cb6453ab9196424e9a209f5122503b2140ef
MBSchneider <matts3@u.washington.edu>
2014-11-04 13:55:37 -0800

    copies most recent extended version to major version

--- sh/mdn4.sh
+++ sh/mdn4.sh
@@ -4,9 +4,13 @@
 if [ "$(git rev-parse --abbrev-ref HEAD)" !=  master ] ; then exit ; fi
 
 lastVer=$(git tag -l | grep '[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}' | sort -n -t . | tail -1)
+lastMajVer=$(git tag -l | sort -r -n -t . | grep -m1 -o '[0-9]\{1,\}\.[0-9]\{1,\}')
 
 # stop here if file alread exists
 if [ -e "dojo-$lastVer.js" ] ; then exit ; fi
 
 cp dojo.js dojo-$lastVer.js
 cp dojo.min.js dojo-$lastVer.min.js
+
+cp dojo.js dojo-$lastMajVer.js
+cp dojo.min.js dojo-$lastMajVer.min.js
