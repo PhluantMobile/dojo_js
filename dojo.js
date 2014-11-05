db3a9315fcc8b4e1d36b9aa9621ffe30d6a330e7
MBSchneider <matts3@u.washington.edu>
2014-11-04 17:33:01 -0800

    bumps to 0.3.3

--- dojo.js
+++ dojo.js
@@ -1,7 +1,7 @@
-/*Dojo.js Framework v0.3.2 | (c) 2014 Phluant, Inc. All rights Reserved | See documentation for more details*/
+/*Dojo.js Framework v0.3.3 | (c) 2014 Phluant, Inc. All rights Reserved | See documentation for more details*/
 (function(){
 	window.dojo = {
-		version: '0.3.2',
+		version: '0.3.3',
 		adInit: null,
 		adIsExpanded: false, /* TODO:  remove this stupid property */
 		closeCallback: null,
