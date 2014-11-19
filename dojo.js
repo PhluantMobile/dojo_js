6abf3600915fa67dc8eb6cb39246f1e511ff071d
MBSchneider <matts3@u.washington.edu>
2014-11-19 12:58:58 -0800

    updates version number

--- dojo.js
+++ dojo.js
@@ -1,7 +1,7 @@
-/*Dojo.js Framework v0.3.4 | (c) 2014 Phluant, Inc. All rights Reserved | See documentation for more details*/
+/*Dojo.js Framework v0.3.5 | (c) 2014 Phluant, Inc. All rights Reserved | See documentation for more details*/
 (function(){
 	window.dojo = {
-		version: '0.3.4',
+		version: '0.3.5',
 		adInit: null,
 		adIsExpanded: false, /* TODO:  remove this stupid property */
 		closeCallback: null,
